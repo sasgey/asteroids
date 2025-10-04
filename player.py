f3425bf44551348bb68fcc6f24b8ce188ef20201
toast <sascha1074@gmail.com>
2025-10-04 13:33:59 +0200

    another typo

--- player.py
+++ player.py
@@ -9,7 +9,7 @@ class Player(CircleShape):
 
     # --- Triangle shape for the ship ---
     def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        forward = pygame.Vector2(0, -1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
